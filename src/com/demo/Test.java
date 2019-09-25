cc33e487932f833448aa5f53422ea6f0c03dedb9
BayMax-GDUT <845625823@qq.com>
2019-09-25 14:57:54 +0800

    javaDemo

---
+++ src/com/demo/Test.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import com.demo.entity.Student;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+    public static void main(String[] args){
+//        Student student = new Student();
+//        student.setId(1);
+//        student.setAge(1);
+//        student.setName("1");
+
+//        Student student = (Student)new ClassPathXmlApplicationContext("applicationContext.xml").getBean("student") ;
+//
+//        System.out.println(student.toString());
+
+    }
+}
