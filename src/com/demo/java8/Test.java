afdb04e9c7cac39f74d8b87400fdf7d72de658e0
BayMax-GDUT <845625823@qq.com>
2021-01-26 17:34:15 +0800

    java8新特性测试类

---
+++ src/com/demo/java8/Test.java
@@ -0,0 +1,15 @@
+package com.demo.java8;
+
+public class Test {
+
+    public static void main(String[] args) {
+        final int num = 1;
+        MathOperation mathOperation = (int a, int b) -> a + b;
+        MathOperation mathOperation1 = (a, b) -> a - b;
+    }
+
+}
+
+interface MathOperation {
+    int operation(int a, int b);
+}
