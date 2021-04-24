f56083c5d125c63daab2ff8c006900af2584449c
BayMax-GDUT <845625823@qq.com>
2021-04-25 00:15:38 +0800

    0424排序算法练习

---
+++ src/com/demo/sort/dailyPractice/date0424/Select.java
@@ -0,0 +1,19 @@
+package com.demo.sort.dailyPractice.date0424;
+
+public class Select {
+
+    public static int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int maxIndex = 0;
+            for (int j = 0; j < array.length - i; j++) {
+                if (array[j] > array[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            int temp = array[array.length - i - 1];
+            array[array.length - i - 1] = array[maxIndex];
+            array[maxIndex] = temp;
+        }
+        return array;
+    }
+}
