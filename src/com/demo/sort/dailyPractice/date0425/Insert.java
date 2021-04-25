6a31885d0dbd15ab2166d3ade2e361b702e61a7c
BayMax-GDUT <845625823@qq.com>
2021-04-25 15:13:31 +0800

    0425排序算法练习

---
+++ src/com/demo/sort/dailyPractice/date0425/Insert.java
@@ -0,0 +1,17 @@
+package com.demo.sort.dailyPractice.date0425;
+
+public class Insert {
+
+    public static int[] sort (int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while (j >= 0 && current < array[j]) {
+                array[j + 1] = array[j];
+                array[j] = current;
+                j--;
+            }
+        }
+        return array;
+    }
+}
