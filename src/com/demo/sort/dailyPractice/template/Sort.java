c6e8a0bae4a746e136059da239f82bd6e0fae713
BayMax-GDUT <845625823@qq.com>
2021-05-10 09:14:42 +0800

    排序算法练习新模板

---
+++ src/com/demo/sort/dailyPractice/template/Sort.java
@@ -0,0 +1,34 @@
+package com.demo.sort.dailyPractice.template;
+
+public class Sort {
+
+    public static int[] bubble(int[] array) {
+
+        return array;
+    }
+
+    public static int[] count(int[] array) {
+
+        return array;
+    }
+
+    public static int[] insert(int[] array) {
+
+        return array;
+    }
+
+    public static int[] quick(int[] array) {
+
+        return array;
+    }
+
+    public static int[] select(int[] array) {
+
+        return array;
+    }
+
+    public static int[] shell(int[] array) {
+
+        return array;
+    }
+}
