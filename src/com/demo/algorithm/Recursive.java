17ec3fb290910d53fc23b762e9576bd6faa6b321
BayMax-GDUT <845625823@qq.com>
2021-05-12 17:35:42 +0800

    斐波那契数列解法进阶

--- src/com/demo/algorithm/Recursive.java
+++ src/com/demo/algorithm/Recursive.java
@@ -6,10 +6,6 @@ import java.util.Arrays;
 
 public class Recursive {
 
-    @Test
-    public static void main(String[] args) {
-    }
-
     /**
      * 汉诺塔问题，递归实现
      * @param n 盘子个数
@@ -17,7 +13,7 @@ public class Recursive {
      * @param y 第二个柱子
      * @param z 第三个柱子
      */
-    private static void hario(int n, String x, String y, String z) {
+    private void hario(int n, String x, String y, String z) {
         if (n < 1) {
             throw new RuntimeException("汉诺塔不能少于1个盘子");
         } else if (n == 1) {
@@ -34,7 +30,7 @@ public class Recursive {
      * @param n
      * @return
      */
-    private static int fibonacci(int n) {
+    private int fibonacci(int n) {
         if (n < 1) {
             throw new RuntimeException("索引不能少于1");
         } else if (n == 1) {
@@ -44,5 +40,44 @@ public class Recursive {
         } else {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
+    }
+
+    /**
+     * 斐波那契数列，进阶递归实现
+     * 避免重复运算（f(19) + f(18) = f(17) + f(18) + f(18)）
+     * @param n
+     * @return
+     */
+    private int fibonacci2(int n) {
+        if (n < 1) return 0;
+        int[] array = new int[n + 1];
+        return recursive(n, array);
+    }
+    private int recursive(int n, int[] array) {
+        if (n == 1 || n == 2) return 1;
+        if (array[n] != 0) return array[n];
+        return recursive(n - 1, array) + recursive(n - 2, array);
+    }
+
+    /**
+     * 斐波那契数列，动态规划实现
+     * 自底向上
+     * @param n
+     * @return
+     */
+    private int fibonacci3(int n) {
+        if (n < 1) return 0;
+        int[] array = new int[n + 1];
+        array[1] = 1;
+        array[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+        return array[n];
+    }
+
+    @Test
+    public void test () {
+        System.out.println(fibonacci3(6));
     }
 }
