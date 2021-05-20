ed0ce8beade3e81a0ec4da26b3da1988f2fa9568
BayMax-GDUT <845625823@qq.com>
2021-05-20 17:27:28 +0800

    0520排序算法练习

--- src/com/demo/sort/dailyPractice/template/Sort.java
+++ src/com/demo/sort/dailyPractice/template/Sort.java
@@ -2,31 +2,80 @@ package com.demo.sort.dailyPractice.template;
 
 public class Sort {
 
+    /**
+     * 冒泡排序
+     * 平均 O(n^2)
+     * 最坏 O(n^2)
+     * 最好 O(n)
+     * 空间 O(1)
+     * 稳定
+     */
     public static int[] bubble(int[] array) {
 
         return array;
     }
 
+    /**
+     * 基数排序
+     * 平均 O(n + k)
+     * 最坏 O(n + k)
+     * 最好 O(n + k)
+     * 空间 O(k)
+     * k是数据范围（max - min + 1）
+     * 稳定
+     */
     public static int[] count(int[] array) {
 
         return array;
     }
 
+    /**
+     * 插入排序
+     * 平均 O(n^2)
+     * 最坏 O(n^2)
+     * 最好 O(n)
+     * 空间 O(1)
+     * 稳定
+     */
     public static int[] insert(int[] array) {
 
         return array;
     }
 
+    /**
+     * 快速排序
+     * 平均 O(n*logn)
+     * 最坏 O(n^2)
+     * 最好 O(n*logn)
+     * 空间 O(logn~n)
+     * 不稳定
+     */
     public static int[] quick(int[] array, int start, int end) {
 
         return array;
     }
 
+    /**
+     * 选择排序
+     * 平均 O(n^2)
+     * 最坏 O(n^2)
+     * 最好 O(n)
+     * 空间 O(1)
+     * 不稳定
+     */
     public static int[] select(int[] array) {
 
         return array;
     }
 
+    /**
+     * 希尔排序
+     * 平均 O(n^1.3)
+     * 最坏 O(n^2)
+     * 最好 O(n)
+     * 空间 O(1)
+     * 不稳定
+     */
     public static int[] shell(int[] array) {
 
         return array;
